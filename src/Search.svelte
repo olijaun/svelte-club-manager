b51f2f7b3fcf78ac50ac64d3707bb69ef3e12823
Oliver Jaun <olijaun@yahoo.com>
2021-02-15 20:34:05 +0100

    store search term and period in store

--- src/Search.svelte
+++ src/Search.svelte
@@ -1,7 +1,8 @@
 <script>
     import {createEventDispatcher, onMount} from 'svelte';
-    import { memberSearchResultStore } from './stores';
-    import { searchMembers, loadPeriods } from './serivce'
+    import {memberSearchResultStore} from './stores';
+    import {searchCriteriaStore} from './stores';
+    import {loadPeriods, searchMembers} from './serivce'
 
     const dispatch = createEventDispatcher();
 
@@ -11,13 +12,6 @@
     let periodsPromise = [];
     let selectedPeriod;
 
-    const unsubscribe = memberSearchResultStore.subscribe(value => {
-        console.log("subscription value: " + JSON.stringify(value));
-        if(value) {
-            membersPromise = Promise.resolve(value);
-        }
-    });
-
     onMount(async () => {
         periodsPromise = loadPeriods().then(p => p.subscriptionPeriods);
     });
@@ -26,35 +20,22 @@
         clearTimeout(timer);
         timer = setTimeout(() => {
             membersPromise = searchMembers(v).then(m => {
-                console.log("storing: " + JSON.stringify(m));
-                memberSearchResultStore.set(m);
+                console.log('storing search term: ' + v);
+                $searchCriteriaStore.term = v;
+                $memberSearchResultStore = m;
                 return m;
             });
         }, 750);
     }
-
-
 
     function memberSelected(id) {
         console.log("dispatch memberSelected: " + id)
         dispatch('memberSelected', id);
     }
 
-    async function loadSubscriptionPeriods() {
-        // const accessToken = await auth0Client.getTokenSilently();
-        //
-        const response = await fetch(`http://localhost:8081/subscription-periods`, {
-            method: 'GET',
-            headers: {
-                //Authorization: `Bearer ${accessToken}`
-            }
-        });
-
-        return response.json().then(p => p.subscriptionPeriods);
-    }
-
     function periodChanged(e) {
         console.log("selected period: " + selectedPeriod);
+        $searchCriteriaStore.selectedPeriod = selectedPeriod;
     }
 </script>
 
@@ -65,20 +46,27 @@
         <p>loading...</p>
 
     {:then periods}
-         <div class="mb-3">
-            <label for="searchStringInput" class="form-label">Search String</label>
-            <input type="searchString" class="form-control" id="searchStringInput"
-                   on:keyup={({ target: { value } }) => debounce(value)}>
-        </div>
-        <div class="mb-3">
-            <label for="periodSelect" class="form-label">Period</label>
-            <select bind:value={selectedPeriod} class="form-control" id="periodSelect" on:change={periodChanged}>
-                <option value="all">*</option>
-                {#each periods as period}
-                    <option value="{period.id}">{period.name}</option>
-                {/each}
-            </select>
-        </div>
+
+        {#if $searchCriteriaStore}
+
+            <div class="mb-3">
+                <label for="searchStringInput" class="form-label">Search String</label>
+                <input type="searchString" class="form-control" id="searchStringInput"
+                       on:keyup={({ target: { value } }) => debounce(value)} value={$searchCriteriaStore.term}>
+            </div>
+            <div class="mb-3">
+                <label for="periodSelect" class="form-label">Period</label>
+                <select bind:value={$searchCriteriaStore.period} class="form-control" id="periodSelect">
+                    <option value="all">*</option>
+                    {#each periods as period}
+                        <option value="{period.id}">{period.name}</option>
+                    {/each}
+                </select>
+            </div>
+
+        {:else}
+            <p>else</p>
+        {/if}
 
     {:catch error}
 
@@ -86,12 +74,10 @@
 
     {/await}
 
-    {#if membersPromise}
-        {#await membersPromise}
-
-            <p>Searching...</p>
-
-        {:then members}
+    {#await membersPromise}
+        <p>loading...</p>
+    {:then unusedValue}
+        {#if $memberSearchResultStore}
 
             <table class="table">
                 <thead>
@@ -106,7 +92,7 @@
 
                 <tbody>
 
-                {#each members.members as member}
+                {#each $memberSearchResultStore.members as member}
                     <tr on:click|preventDefault={e => memberSelected(member.id)}>
                         <th scope="row">{member.id}</th>
                         <td>{member.lastNameOrCompanyName}</td>
@@ -122,13 +108,10 @@
                     </tr>
                 {/each}
 
-
                 </tbody>
             </table>
-
-        {:catch error}
-            <p>Error when searching: {error}</p>
-        {/await}
-    {/if}
-
+        {/if}
+    {:catch error}
+        <p>failed to search: {error}</p>
+    {/await}
 </div>
--- src/stores.js
+++ src/stores.js
@@ -1,3 +1,5 @@
 import { writable } from 'svelte/store';
 
 export const memberSearchResultStore = writable(undefined);
+
+export const searchCriteriaStore = writable({ term: "", period: "all"});
